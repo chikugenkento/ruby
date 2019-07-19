cf1cab135eb6479bada2cb99ef487a73623e2210
kento chikugen <knt37007@gmail.com>
2019-07-19 07:41:22 +0000

    ff3

---
+++ hello2.rb
@@ -0,0 +1,12 @@
+puts "はじめてのルビー"
+puts "始めました"+"ルビー"
+p 3+3
+p 3-3
+p 3*3
+p 3/3
+num_array = "1","2","3","4"
+p num_array
+name = { namae: 'chikugenkent',S: '19920211', K 'A'} 
+p name[:namae]
+p name[:s]
+p name[k]
